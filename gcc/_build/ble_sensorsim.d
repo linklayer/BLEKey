d0183ee240a0fc3f32903df4677423c28d2cffcb
Eric Evenchick <eric@evenchick.com>
2015-03-04 17:14:16 -0500

    ignore build artifacts

--- gcc/_build/ble_sensorsim.d
+++
@@ -1,7 +0,0 @@
-_build/ble_sensorsim.o: ../../../../../Source/ble/ble_sensorsim.c \
- ../../../../../Include/ble/ble_sensorsim.h \
- /Users/eric/Documents/gcc-arm-none-eabi-4_8-2014q3/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
- /Users/eric/Documents/gcc-arm-none-eabi-4_8-2014q3/arm-none-eabi/include/stdint.h \
- /Users/eric/Documents/gcc-arm-none-eabi-4_8-2014q3/arm-none-eabi/include/machine/_default_types.h \
- /Users/eric/Documents/gcc-arm-none-eabi-4_8-2014q3/arm-none-eabi/include/sys/features.h \
- /Users/eric/Documents/gcc-arm-none-eabi-4_8-2014q3/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h
