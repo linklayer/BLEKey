ad5a1bbd6a553a7923e93c0955c0da421ea91427
Mark Baseggio <Mark@Accuvant-Air-MAB.local>
2015-05-30 17:31:39 -0400

    fixed pins for serial again.. duh

--- retarget.c
+++ retarget.c
@@ -15,5 +15,5 @@ int _write(int fd, char * str, int len)
 void retarget_init(void)
 {
     //simple_uart_config(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, HWFC);
-    simple_uart_config(RTS_PIN_NUMBER, 11, CTS_PIN_NUMBER, 9, HWFC);
+    simple_uart_config(RTS_PIN_NUMBER, 9, CTS_PIN_NUMBER, 11, HWFC);
 }
