7d0def6a8cb01705b53ea20ce09a264443deddba
Eric Evenchick <eric@evenchick.com>
2015-06-07 13:52:25 -0400

    adding cards to struct for BLE transmision

--- wiegand.c
+++ wiegand.c
@@ -23,21 +23,21 @@
 #define MAX_BITS 100
 #define MAX_LEN 44
 
-volatile uint64_t card_data = 0; 
+volatile uint64_t card_data = 0;
 volatile uint8_t data_bits[MAX_BITS];
 volatile uint8_t bit_count = 0;
 volatile bool data_incoming = false;
 volatile bool data_ready = false;
 volatile bool timer_started = false;
-volatile bool card_fubar = false;	// set if BLE screws up an incoming card
+volatile bool card_fubar = false;       // set if BLE screws up an incoming card
 volatile uint32_t timerStop;
 
 static struct wiegand_ctx *p_ctx;
 
 static const uint16_t padding[19] =
 {
-	0, 0, 0, 0, 0, 0, 0, 0, 0x3, 0x5, 0x9, 0x11, 0x21,
-	0x41, 0x81, 0x101, 0x201, 0x401, 0x801
+    0, 0, 0, 0, 0, 0, 0, 0, 0x3, 0x5, 0x9, 0x11, 0x21,
+    0x41, 0x81, 0x101, 0x201, 0x401, 0x801
 };
 
 void wiegand_init(struct wiegand_ctx *ctx)
@@ -47,11 +47,11 @@ void wiegand_init(struct wiegand_ctx *ctx)
     retarget_init(); // retarget printf to UART pins 9(tx) and 11(rx)
     printf("Initializing wiegand shit...");
 
-	// Set the control lines as outputs and pull them low
-	nrf_gpio_cfg_output(DATA0_CTL);
-	nrf_gpio_cfg_output(DATA1_CTL);
-	nrf_gpio_pin_clear(DATA0_CTL);
-	nrf_gpio_pin_clear(DATA1_CTL);
+    // Set the control lines as outputs and pull them low
+    nrf_gpio_cfg_output(DATA0_CTL);
+    nrf_gpio_cfg_output(DATA1_CTL);
+    nrf_gpio_pin_clear(DATA0_CTL);
+    nrf_gpio_pin_clear(DATA1_CTL);
 
     printf("Pin interrupts...");
     // Set up GPIO and pin interrupts
@@ -84,9 +84,9 @@ void wiegand_init(struct wiegand_ctx *ctx)
 }
 
 void add_card(uint8_t *data, uint8_t len) {
-    // shift cards over 
-    memcpy(&(p_ctx->card_store[1]), p_ctx->card_store, 
-	   sizeof(p_ctx->card_store) - sizeof(struct card));
+    // shift cards over
+    memcpy(&(p_ctx->card_store[1]), p_ctx->card_store,
+           sizeof(p_ctx->card_store) - sizeof(struct card));
 
     // add card to store
     p_ctx->card_store[0].bit_len = len;
@@ -96,8 +96,8 @@ void add_card(uint8_t *data, uint8_t len) {
 
 void wiegand_task(void)
 {
-	if (data_incoming && !timer_started) {
-		NRF_TIMER2->TASKS_START = 1;    // Start TIMER2
+    if (data_incoming && !timer_started) {
+        NRF_TIMER2->TASKS_START = 1;    // Start TIMER2
         timer_started = true;
     }
 
@@ -105,17 +105,19 @@ void wiegand_task(void)
         if (bit_count > 1 && !card_fubar)   // avoid garbage data at startup.
         {
             uint8_t pad_len = (MAX_LEN - bit_count);
-			printf("Read %d bits: ", bit_count);
+            printf("Read %d bits: ", bit_count);
 
-			uint64_t card_val = padding[pad_len];
-			card_val <<= bit_count;
-			card_val |= card_data;
+            uint64_t card_val = padding[pad_len];
+            card_val <<= bit_count;
+            card_val |= card_data;
 
             for (uint8_t i=0; i<bit_count; i++)
             {
                 printf("%d", data_bits[i]);
             }
-			printf( " 0x%llx \r\n", card_val);
+            printf( " 0x%llx \r\n", card_val);
+	    // add card to struct for BLE transmission
+	    add_card(&card_val, bit_count);
 
         }
 
@@ -123,8 +125,8 @@ void wiegand_task(void)
         timer_started = false;
         bit_count = 0;
         data_ready = false;
-		card_data = 0;
-		card_fubar = false;
+        card_data = 0;
+        card_fubar = false;
 
         // clears the old data
         for (uint8_t i=0; i<MAX_BITS; i++)
@@ -140,30 +142,30 @@ void TIMER2_IRQHandler(void)
     {
         data_ready = true;
         NRF_TIMER2->EVENTS_COMPARE[0] = 0;           // Clear compare register 0 event
-        NRF_TIMER2->TASKS_STOP = 1;     // Stop the timer 
-		NRF_TIMER2->TASKS_CAPTURE[1] = 1;
+        NRF_TIMER2->TASKS_STOP = 1;     // Stop the timer
+        NRF_TIMER2->TASKS_CAPTURE[1] = 1;
         NRF_TIMER2->CC[0] = (NRF_TIMER2->CC[1] + TIMER_DELAY);
 
     }
 }
 
-void GPIOTE_IRQHandler(void) 
+void GPIOTE_IRQHandler(void)
 {
     // This handler will be run after wakeup from system ON (GPIO wakeup)
-	uint32_t port_status = NRF_GPIO->IN;
-	NRF_GPIOTE->EVENTS_PORT = 0;    // Clear event
-	card_data <<= 1;
+    uint32_t port_status = NRF_GPIO->IN;
+    NRF_GPIOTE->EVENTS_PORT = 0;    // Clear event
+    card_data <<= 1;
     if (!(port_status >> DATA1_IN & 1UL)) {
         data_bits[bit_count] = 1;
-		card_data |= 1;
+        card_data |= 1;
     } else if (!(port_status >> DATA0_IN & 1UL)) {
         data_bits[bit_count] = 0;
     } else {
-		// port status lost thanks to BLE delaying read.
-		data_bits[bit_count] = 9;
-		card_fubar = true;
-	}
-	data_incoming = true;
+        // port status lost thanks to BLE delaying read.
+        data_bits[bit_count] = 9;
+        card_fubar = true;
+    }
+    data_incoming = true;
     NRF_TIMER2->TASKS_CAPTURE[1] = 1;   // trigger CAPTURE task
     NRF_TIMER2->CC[0] = (NRF_TIMER2->CC[1] + TIMER_DELAY); // Reset timer
     bit_count++;
