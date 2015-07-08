b601cbcf337d5ae189202c2b4ac42442b6645bf5
blark <mark@basegg.io>
2015-07-08 08:35:58 -0400

    intergace stuff

--- client/blekey.py
+++ client/blekey.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python2
 import cmd
 import pygatt
 import pprint as pp
 import os
 
 DEFAULT_MAC = "D4:34:E8:CA:6F:6A"
+DEFAULT_TIMEOUT = 15
 
 
 class BLEKeyClient(cmd.Cmd):
     """Derp de derp"""
 
-    macs = []
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.macs = []
+        self.prompt = '\033[1;30m[n/c]\033[1;m blekey> '
 
     def emptyline(self):
         pass
@@ -22,14 +27,15 @@ class BLEKeyClient(cmd.Cmd):
             self.macs.append(item['address'])
 
     def help_scan(self):
-        print("scan help goes here")
+        print("The scan command uses hcitool to probe for BLE devices.")
 
     def do_connect(self, mac):
         if not mac:
             mac = DEFAULT_MAC
         print("connecting to %s" % mac)
         self.bk = pygatt.BluetoothLEDevice(mac, app_options="-t random")
-        self.bk.connect(timeout=10)
+        self.bk.connect(timeout=DEFAULT_TIMEOUT)
+        self.prompt = "\033[1;34m[%s]\033[1;m blekey>" % mac
 
     def complete_connect(self, text, line, begidx, endidx):
         if not text:
@@ -41,26 +47,57 @@ class BLEKeyClient(cmd.Cmd):
                            ]
         return completions
 
+    def help_connect(self):
+        print("Usage: connect <MAC_ADDRESS>")
+        print("If you don't know the device's MAC try using scan first.\
+              Tab completion is your friend.")
+
     def do_tx(self, data):
         print("sending...")
         self.bk.char_write(0x0d, [0x01])
 
+    def help_tx(self):
+        print("Currently the tx command sends the card read by BLEKey")
+
     def do_readcards(self, _):
         print("reading last cards...")
-        last_cards = self.bk.char_read_hnd(0x0b)
+        last_cards = self.bk.char_read_hnd(0x0b, timeout=DEFAULT_TIMEOUT)
         for i in range(0, 3):
             start = i * 7
-            print ("Bits: %d" % last_cards[start])
+            print ("%d. %d bit card:" % (i, last_cards[start])),
             card_data = reversed(last_cards[start + 1:start + 6])
             fixed = ''.join('{:02x}'.format(x) for x in card_data)
             print ("0x%s" % fixed)
             fixed = None
             card_data = None
 
+    def help_readcards(self):
+        print("readcards reads the last three cards")
+
+    def do_disconnect(self, _):
+        try:
+            self.bk.disconnect()
+            self.prompt = '\033[1;30m[n/c]\033[1;m blekey> '
+        except AttributeError:
+            print("There is no connection to disconnect.")
+
+    def help_disconnect(self):
+        print("Usage: disconnect")
+        print("Disconnects gatttool from the BLE device.")
+
     def do_EOF(self, line):
+        try:
+            self.bk.disconnect()
+            del self.bk
+        except AttributeError:
+            pass
         return True
 
+    def help_EOF(self):
+        print("Use exit, quit or ^D to quit the client.")
+
     do_exit = do_quit = do_EOF
+    help_exit = help_quit = help_EOF
 
 if __name__ == '__main__':
     if os.getuid() is not 0:
